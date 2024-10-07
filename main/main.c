0a10567415f25eeccbf768dd10dcb2abd21ef42c
luiz <luizgfa@al.insper.edu.br>
2024-10-07 18:43:33 +0000

    lab

--- main/main.c
+++ main/main.c
@@ -5,57 +5,52 @@
 
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "mpu6050.h"
-
 #include <Fusion.h>
 
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+#define SAMPLE_PERIOD (0.01f)
 
 static void mpu6050_reset() {
-    // Two byte reset. First byte register, second byte data
-    // There are a load more options to set up the device in different ways that could be added here
-    uint8_t buf[] = {0x6B, 0x00};
+    uint8_t buf[] = {0x6B, 0x00}; // Reset
     i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
 }
 
 static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
-
     uint8_t buffer[6];
-
-    // Start reading acceleration registers from register 0x3B for 6 bytes
     uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
 
+    // Lê os dados de aceleração
+    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
     for (int i = 0; i < 3; i++) {
         accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
-    // Now gyro data from reg 0x43 for 6 bytes
-    // The register is auto incrementing on each read
+    // Lê os dados do giroscópio
     val = 0x43;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);  // False - finished with bus
-
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
     for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
-    // Now temperature from reg 0x41 for 2 bytes
-    // The register is auto incrementing on each read
+    // Lê a temperatura
     val = 0x41;
     i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);  // False - finished with bus
-
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);
     *temp = buffer[0] << 8 | buffer[1];
+}
+
+void send_data(int axis, int value) {
+    uart_putc(UART0_BASE, axis);      // Enviar o eixo
+    uart_putc(UART0_BASE, (value >> 8) & 0xFF);       // Enviar o byte mais significativo
+    uart_putc(UART0_BASE, value & 0xFF);              // Enviar o byte menos significativo
+    uart_putc(UART0_BASE, 0xFF);                      // Enviar o byte de fim de pacote
 }
 
 void mpu6050_task(void *p) {
@@ -64,29 +59,48 @@ void mpu6050_task(void *p) {
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
-
     mpu6050_reset();
+
     int16_t acceleration[3], gyro[3], temp;
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
 
     while(1) {
+        // Lê os dados da IMU
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+
+        // Converte os valores
+        FusionVector gyroscope = {
+            .axis.x = gyro[0] / 131.0f,
+            .axis.y = gyro[1] / 131.0f,
+            .axis.z = gyro[2] / 131.0f,
+        };
+        FusionVector accelerometer = {
+            .axis.x = acceleration[0] / 16384.0f,
+            .axis.y = acceleration[1] / 16384.0f,
+            .axis.z = acceleration[2] / 16384.0f,
+        };
+
+        // Atualiza a fusão de dados
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+        FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        // Envia os dados da aceleração e giroscópio pela UART
+        //send_data(0, euler.angle.pitch);  // Eixo X do giroscópio
+        send_data(0, -euler.angle.yaw);  // Eixo Y do giroscópio
+        send_data(1, -euler.angle.roll);  // Eixo Z do giroscópio
+        if (accelerometer.axis.y > 1.5) {
+            send_data(2, 1);
+        } 
+        // Detecta movimento brusco na horizontal (para simular o clique)
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
-
-
 }
 
 int main() {
     stdio_init_all();
-
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
-
+    xTaskCreate(mpu6050_task, "mpu6050_task", 8192, NULL, 1, NULL);
     vTaskStartScheduler();
-
-    while (true)
-        ;
+    while (true);
 }
